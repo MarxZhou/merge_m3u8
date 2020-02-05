944010fe0fb58da32c40185b7052f1443814f256
MarxZhou <marxzhou@aliyun.com>
2020-02-06 01:23:28 +0800

    修复BUG

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const chalk = require('chalk');
-const chance = require('chance');
 
 const { execSync } = require('child_process');
 
@@ -13,21 +12,22 @@ if (!fs.existsSync(tempPath)) {
 const m3u8Files = fs.readdirSync(inputPath).filter(value => value.endsWith('.m3u8'));
 
 m3u8Files.forEach(value => {
-  const newFileName = value.replace(/[^\u4e00-\u9fa5A-Za-z0-9_.]/, '');
+  const newFileName = value.replace(/[^\u4e00-\u9fa5A-Za-z0-9_.\-]/g, '');
 
   fs.copyFileSync(`${inputPath}/${value}`, `${tempPath}/${newFileName}`);
 });
 
-const tempM3u8Files = fs.readdirSync(tempPath).filter(value => value.endsWith('.m3u8'));
+let tempM3u8Files = fs.readdirSync(tempPath).filter(value => value.endsWith('.m3u8'));
+
+process.chdir(tempPath);
 
 tempM3u8Files.forEach(value => {
-  const m3u8File = fs.readFileSync(`${inputPath}/${value}`, 'utf8');
+  console.log('value:', value);
+  const m3u8File = fs.readFileSync(`${tempPath}/${value}`, 'utf8');
 
   let keyUri = '';
   let newKeyUri = '';
   let newM3u8File;
-
-  const guid = value || chance.guid();
 
   const isEncrypted = m3u8File.match(/URI="(.*)"/);
 
@@ -42,40 +42,14 @@ tempM3u8Files.forEach(value => {
   const tempFile = `${tempPath}/${value}`;
 
   fs.writeFileSync(tempFile, newM3u8File, { flag: 'r+' });
+});
 
+tempM3u8Files.forEach(value => {
   console.log(`${chalk.blueBright('正在转换中：')}${value}`);
 
-  const outputFilePath = `${outputPath}/${value.slice(0, value.length - 5) || guid}.${outputFileType}`;
-
-  if (fs.existsSync(outputFilePath)) {
-    fs.unlinkSync(outputFilePath);
-  }
-
-  process.chdir(tempPath);
+  const outputFilePath = `${outputPath}/${value.slice(0, value.length - 5)}.${outputFileType}`;
 
   execSync(`ffmpeg -allowed_extensions ALL -i ${value} ${outputFilePath}`);
 
-  process.chdir(__dirname);
-
-  console.log(`${chalk.greenBright('文件转换完成：')}${guid}`);
+  console.log(`${chalk.greenBright('文件转换完成：')}${value}`);
 });
-
-const deleteFolderRecursive = path => {
-  if (!fs.accessSync(path)) {
-    fs.readdirSync(path).forEach(file => {
-      var curPath = path + '/' + file;
-      if (fs.lstatSync(curPath).isDirectory()) {
-        // recurse
-        deleteFolderRecursive(curPath);
-      } else {
-        // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
-  }
-};
-
-if (!saveM3u8File) {
-  deleteFolderRecursive(tempPath);
-}
