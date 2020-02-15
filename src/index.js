b252b4e6faf5cdf646fbb8c2b18d28179ddb60bd
MarxZhou <marxzhou@aliyun.com>
2020-02-16 01:30:09 +0800

    大幅调整了整体的执行逻辑，并修正了采用的错误的API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
+const { deleteFolderRecursive, Log } = require('./utils');
+
 const { execSync } = require('child_process');
 
 const { inputPath, outputPath, tempPath, saveM3u8File, outputFileType } = require('./config');
@@ -8,6 +10,7 @@ const { inputPath, outputPath, tempPath, saveM3u8File, outputFileType } = requir
 // 判断临时文件夹是否存在，如果不存在则进行创建
 if (!fs.existsSync(tempPath)) {
   fs.mkdirSync(tempPath);
+  Log.greenBright('临时文件夹创建完成：', tempPath);
 }
 
 // 读取m3u8的元数据
@@ -23,6 +26,7 @@ m3u8Files.forEach(value => {
 
   fs.copyFileSync(`${inputPath}/${value}`, `${tempPath}/${newFileName}`);
 });
+Log.greenBright('临时文件创建完成，文件名中的除中文、英文、数字、下划线、点和中横线外的字符都会被删除');
 
 // 读取重命名完成的临时文件
 let tempM3u8Files = fs.readdirSync(tempPath);
@@ -30,7 +34,7 @@ let tempM3u8Files = fs.readdirSync(tempPath);
 // 调整当前工作目录为临时文件夹
 process.chdir(tempPath);
 
-const keyReg = /URI="(.*)"/;
+const keyReg = /URI="(.*)\/(\w*)\/(\w*)"/;
 
 /**
  * 进行密钥路径修正
@@ -43,66 +47,71 @@ tempM3u8Files.forEach(value => {
   const isEncrypted = m3u8File.match(keyReg);
 
   if (isEncrypted) {
-    // 加密：
+    // 已加密
 
     // 处理密钥的路径
-    const keyUri = m3u8File.match(keyReg)[1].split('/');
+    const keyUri = isEncrypted[1];
 
-    const keyUriLength = keyUri.length;
-
-    const newKeyUri = `${inputPath}/${keyUri.slice(keyUriLength - 2).join('/')}`;
-
-    const newM3u8File = m3u8File.replace(keyReg, `URI="${newKeyUri}"`);
+    const newM3u8File = m3u8File.replace(keyUri, inputPath);
 
     const tempFile = `${tempPath}/${value}`;
 
-    fs.writeFileSync(tempFile, newM3u8File, { flag: 'r+' });
+    fs.writeFileSync(tempFile, newM3u8File);
+
+    Log.greenBright('文件密钥信息修正完成：', value);
+  } else {
+    // 未加密
+
+    Log.greenBright('文件未加密：', value);
   }
 });
 
-const pathReg = /\/(.*)\/\/?(\w*)\/(\w*\s)/g;
+const pathReg = /(\/.*\w)(\/\/?)(\w*)\/(\w*\s)/;
 
 /**
  * 进行元数据路径修正
  */
-tempM3u8Files.forEach((value, index) => {
-  const m3u8File = fs.readFileSync(`${tempPath}/${value}`, 'utf8');
-
-  if (index === 1) {
-    console.log('value:', value);
+tempM3u8Files.forEach(value => {
+  Log.blueBright('正在处理m3u8文件元数据：', value);
+  try {
+    const m3u8File = fs.readFileSync(`${tempPath}/${value}`, 'utf8');
 
     const basePath = m3u8File.match(pathReg);
-    console.log('basePath:', basePath);
+
+    const path = basePath[1];
+
+    const errSign = basePath[2] === '//' ? '//' : null;
+
+    const temp = m3u8File.replace(new RegExp(path, 'g'), inputPath);
+
+    let newM3u8File = temp;
+    if (errSign) {
+      newM3u8File = temp.replace(new RegExp(errSign, 'g'), '/');
+    }
+
+    const tempFile = `${tempPath}/${value}`;
+
+    fs.writeFileSync(tempFile, newM3u8File);
+    Log.greenBright('m3u8文件元数据修正完成：', value);
+  } catch (e) {
+    Log.redBright('m3u8文件元数据出现了错误：', `${value}：${JSON.stringify(e)}`);
   }
-
-  /*
-  const pathArr = basePath.split('/');
-  console.log('pathArr:', pathArr);
-
-  const oldPath =
-    '/' +
-    pathArr
-      .slice(0, pathArr.length - 2)
-      .filter(value => value)
-      .join('/');
-  console.log('oldPath:', oldPath);
-*/
-
-  // const newM3u8File = m3u8File.replace(/111/, '');
-
-  // const tempFile = `${tempPath}/${value}`;
-
-  // fs.writeFileSync(tempFile, newM3u8File, { flag: 'r+' });
-  // let newM3u8File = m3u8File.replace();
 });
-/*
+
 tempM3u8Files.forEach(value => {
-  console.log(`${chalk.blueBright('正在转换中：')}${value}`);
+  Log.blueBright('正在转换中：', value);
 
   const outputFilePath = `${outputPath}/${value.slice(0, value.length - 5)}.${outputFileType}`;
 
-  execSync(`ffmpeg -allowed_extensions ALL -i ${value} ${outputFilePath}`);
-
-  console.log(`${chalk.greenBright('文件转换完成：')}${value}`);
+  try {
+    execSync(`ffmpeg -allowed_extensions ALL -i ${value} ${outputFilePath}`);
+    Log.blueBright('文件转换完成：', value);
+  } catch (e) {
+    Log.redBright('文件转换出现了错误：', `${value}：${JSON.stringify(e)}`);
+  }
 });
-*/
+
+if (!saveM3u8File) {
+  deleteFolderRecursive(tempPath);
+  Log.greenBright('临时文件夹删除完成');
+}
